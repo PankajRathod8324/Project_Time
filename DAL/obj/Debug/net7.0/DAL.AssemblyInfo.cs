aca48b6ddba2769c758172ddcc7c08753ad5bff6
Pankaj0083 <tatvasoft.pct71@outlook.com>
2025-05-11 23:42:50 +0530

    Cancelbnt fixed

--- DAL/obj/Debug/net7.0/DAL.AssemblyInfo.cs
+++ DAL/obj/Debug/net7.0/DAL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a14cc7acad20f2fb24f281ae641b07948cac9e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
